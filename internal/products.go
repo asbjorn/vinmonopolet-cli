a68b7f1067a3e57e33bd8308c9ed979212d57195
Asbjørn Alexander Fellinghaug <asbjorn@fellinghaug.com>
2020-10-30 12:14:43 +0100

    Changed to use api.vinmonopolet.no instead

--- internal/products.go
+++ internal/products.go
@@ -3,14 +3,11 @@ package internal
 const SEARCH_API_URL = "https://www.vinmonopolet.no/api/search"
 
 /*
+Use the API from https://api.vinmonopolet.no/developer
 
-COOKIES IN RESPONSE:
-Cookie: JSESSIONID=Y2-4db41fad-0c14-4da5-97ee-8f088cfe780a; QueueITAccepted-SDFrts345E-V3_frankrikehellas2020=QueueId%3D00000000-0000-0000-0000-000000000000%26IsCookieExtendable%3Dtrue%26Expires%3D1604054993%26Hash%3D392debf24ea8f0d6c2b9a11a00345b8817a3c3a5d81074ccf82492241fa977fe
+Will retrieve an API Key by signing up on their site!
+*/
 
-QUERY STRING PARAMETERS
-q: :relevance:visibleInSearch:true:mainCategory:øl
-searchType: product
-currentPage: 0
-fields: FULL
-pageSize: 24
+/*
+https://api.vinmonopolet.no/docs/services/products/operations/GET_STOCK_ACCUMULATED
 */
